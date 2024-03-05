f6a95d77685c8defa33e67f21c9cbb2b9f169cdd
Chi4n <gianb04@gmail.com>
2024-03-05 14:23:44 -0300

    estilo

--- app.js
+++ app.js
@@ -1,102 +1,111 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("mainForm");
+  const name = document.getElementById("name").focus();
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const password = document.getElementById("password");
+  const message = document.getElementById("message");
 
-  const form = document.getElementById('mainForm');
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
-  const phone = document.getElementById('phone');
-  const password = document.getElementById('password');
-  const message = document.getElementById('message');
-
-  form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      if (checkInputs()) {
-          showModal();
-      }
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (checkInputs()) {
+      showModal();
+    }
   });
 
-  name.addEventListener('input', () => {
-      validateField(name, name.value.trim() !== '', 'Name cannot be blank');
+  name.addEventListener("input", () => {
+    validateField(name, name.value.trim() !== "", "Name cannot be blank");
   });
 
-  email.addEventListener('input', () => {
-      validateField(email, isEmail(email.value.trim()), 'Not a valid email');
+  email.addEventListener("input", () => {
+    validateField(email, isEmail(email.value.trim()), "Not a valid email");
   });
 
-  phone.addEventListener('input', () => {
-      validateField(phone, isPhone(phone.value.trim()), 'Not a valid phone number');
+  phone.addEventListener("input", () => {
+    validateField(
+      phone,
+      isPhone(phone.value.trim()),
+      "Not a valid phone number"
+    );
   });
 
-  password.addEventListener('input', () => {
-      validateField(password, password.value.trim().length >= 8, 'Password must be at least 8 characters');
+  password.addEventListener("input", () => {
+    validateField(
+      password,
+      password.value.trim().length >= 8,
+      "Password must be at least 8 characters"
+    );
   });
 
-  message.addEventListener('input', () => {
-      validateField(message, message.value.trim() !== '', 'Message cannot be blank');
+  message.addEventListener("input", () => {
+    validateField(
+      message,
+      message.value.trim() !== "",
+      "Message cannot be blank"
+    );
   });
 
   function checkInputs() {
-      let isValid = true;
-      validateField(name, name.value.trim() !== '', 'Name cannot be blank');
-      validateField(email, isEmail(email.value.trim()), 'Not a valid email');
-      validateField(phone, isPhone(phone.value.trim()), 'Not a valid phone number');
-      validateField(password, password.value.trim().length >= 8, 'Password must be at least 8 characters');
-      validateField(message, message.value.trim() !== '', 'Message cannot be blank');
+    let isValid = true;
+    validateField(name, name.value.trim() !== "", "Name cannot be blank");
+    validateField(email, isEmail(email.value.trim()), "Not a valid email");
+    validateField(phone,isPhone(phone.value.trim()),"Not a valid phone number");
+    validateField(password,password.value.trim().length >= 8,"Password must be at least 8 characters");
+    validateField(message,message.value.trim() !== "","Message cannot be blank");
 
-      document.querySelectorAll('.form-control').forEach((control) => {
-          if (control.classList.contains('error')) {
-              isValid = false;
-          }
-      });
+    document.querySelectorAll(".form-control").forEach((control) => {
+      if (control.classList.contains("error")) {
+        isValid = false;
+      }
+    });
 
-      return isValid;
-
+    return isValid;
   }
 
   function validateField(input, condition, errorMessage) {
-      if (condition) {
-          setSuccess(input);
-      } else {
-          setError(input, errorMessage);
-      }
+    if (condition) {
+      setSuccess(input);
+    } else {
+      setError(input, errorMessage);
+    }
   }
 
   function setError(input, message) {
-      const formControl = input.parentElement;
-      const icon = formControl.querySelector('.icon');
-      formControl.className = 'form-control error';
-      icon.className = 'icon fas fa-times-circle';
-      input.placeholder = message;
+    const formControl = input.parentElement;
+    const icon = formControl.querySelector(".icon");
+    formControl.className = "form-control error";
+    icon.className = "icon fas fa-times-circle";
+    input.placeholder = message;
   }
 
   function setSuccess(input) {
-      const formControl = input.parentElement;
-      const icon = formControl.querySelector('.icon');
-      formControl.className = 'form-control success';
-      icon.className = 'icon fas fa-check-circle';
+    const formControl = input.parentElement;
+    const icon = formControl.querySelector(".icon");
+    formControl.className = "form-control success";
+    icon.className = "icon fas fa-check-circle";
   }
 
   function isEmail(email) {
-      return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
+    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email);
   }
 
   function isPhone(phone) {
-      return /^\+?(\d.*){3,}$/.test(phone);
+    return /^\+?(\d.*){3,}$/.test(phone);
   }
 
   function showModal() {
-      const modal = document.getElementById('successModal');
-      modal.style.display = 'block';
+    const modal = document.getElementById("successModal");
+    modal.style.display = "block";
 
-      const closeBtn = document.querySelector('.close-button');
-      closeBtn.onclick = function () {
-          modal.style.display = 'none';
-      };
+    const closeBtn = document.querySelector(".close-button");
+    closeBtn.onclick = function () {
+      modal.style.display = "none";
+    };
 
-      window.onclick = function (event) {
-          if (event.target === modal) {
-              modal.style.display = 'none';
-          }
-      };
+    window.onclick = function (event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
   }
-
 });
